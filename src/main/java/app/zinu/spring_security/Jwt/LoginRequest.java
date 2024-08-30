0175a3f3eac6eb45760a3dc71770fa4f95bb0958
zinujust <zainalshariff@Zainals-MacBook-Pro.local>
2024-08-30 00:28:41 -0700

    Added AuthEntryPoint, LoginResponse, LoginRequest classes and created 'authenticateUser' endpoint

---
+++ src/main/java/app/zinu/spring_security/Jwt/LoginRequest.java
@@ -0,0 +1,16 @@
+package app.zinu.spring_security.Jwt;
+
+public class LoginRequest {
+
+  private String username;
+
+  private String password;
+
+  public String getPassword() { return password; }
+
+  public void setPassword(String password) { this.password = password; }
+
+  public String getUsername() { return username; }
+
+  public void setUsername(String username) { this.username = username; }
+}
